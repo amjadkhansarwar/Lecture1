8ea49cbe2bcd901f995c12672c1e4207849e1af1
amjadkhansarwar <muhammad.amjad@iths.se>
2023-09-07 11:06:21 +0200

    making my first lecture repo

---
+++ Lecture1/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+int age = 2;
+if (age > 40)
+{
+    Console.WriteLine(" Du are middle age person");
+    Console.WriteLine(" Its ok");
+}
+  Console.WriteLine("Its important you are learning");
+*/
+
+/*int age = 52;
+string name = "Amjad";
+if(age > 40 && name == "Amjad")
+{
+    Console.WriteLine(" you need to work hard");
+
+}
+else if (age > 40 || name == "Ali")
+{
+    Console.WriteLine(" you need to do your job");
+}
+ */
+
+/*int num = 5;
+string myName = "amjad";
+
+if (name)
+
+*/
+
+/*int num = 2;
+ switch (num)
+{
+    case 1:
+        Console.WriteLine("This is one number");
+     breake;
+}*/
+int age = 33;
+
+//om <18
+//  skriver man ut
+// Barn/ungdom
+
+if (age < 18)
+{
+    Console.WriteLine("Barn/Un´ungdum");
+}
+
+//om >18 men < 65
+
+// skriver man ut Vuxen
+
+else if (age > 18 && age < 65)
+{
+    Console.WriteLine(" Vuxan");
+}
+
+// Annars
+
+//skriver man ut Pensionär
+
+
+else
+    {
+    Console.WriteLine(" Pensionär");
+   }
